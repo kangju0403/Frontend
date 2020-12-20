301cb7e6aff163dcbe4b9b50c3003faae197d4be
kangju0403 <74080888+kangju0403@users.noreply.github.com>
2020-12-20 16:32:31 -0700

    responsive website(desktop/mobile)
    Only needs edits on personal and background images

---
+++ js/main.js
@@ -0,0 +1,8 @@
+if(screen.width<=699){document.location="../mobile.html"}
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
